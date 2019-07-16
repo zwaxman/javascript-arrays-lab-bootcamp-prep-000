f242c33cd472ec345e30f58752222dc63cbb3bf9
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 23:32:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  newK=kittens.slice(0,kittens.length)
+  newK=kittens.slice(0,kittens.length-1)
 }
 //   describe('removeLastKitten()', function() {
 //     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
