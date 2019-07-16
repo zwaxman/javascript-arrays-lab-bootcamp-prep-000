b5796c40dae0601adb0e1566e9b0df9ef8008748
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 23:25:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 function destructivelyAppendKitten(name){
   kittens.push(name)
+  return kittens
 }
 
 //   describe('destructivelyAppendKitten(name)', function() {
