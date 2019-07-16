33376242190168261647db53a6d59804906ebc4e
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 23:27:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ function destructivelyAppendKitten(name){
   return kittens
 }
 
-//   describe('destructivelyAppendKitten(name)', function() {
-//     it('appends a kitten to the end of the kittens array', function() {
-//       window.kittens = ['Milo', 'Otis', 'Garfield'];
-//       destructivelyAppendKitten('Ralph')
-//       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
-//     })
-//   })
-
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
 //   describe('destructivelyPrependKitten(name)', function() {
 //     it('prepends a kitten to the beginning of the kittens array', function() {
 //       window.kittens = ['Milo', 'Otis', 'Garfield'];
